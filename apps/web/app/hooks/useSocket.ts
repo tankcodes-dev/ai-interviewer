077acfd2fad3f612c7aeda56086595279a33e691
Shwetank Agarwal <shwetank2003@gmail.com>
2026-01-13 22:57:18 +0530

    bug fix

--- apps/web/app/hooks/useSocket.ts
+++ apps/web/app/hooks/useSocket.ts
@@ -1,5 +1,5 @@
 // ws.ts
-export const ws = new WebSocket(`${process.env.WS_URL}`);
+export const ws = new WebSocket(`${process.env.NEXT_PUBLIC_WS_URL}`);
 
 ws.onopen = () => console.log("WebSocket connected");
 ws.onclose = () => console.log("WebSocket disconnected");
