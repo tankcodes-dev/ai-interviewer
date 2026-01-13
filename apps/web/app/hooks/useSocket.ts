450bb16a7e683d960deb278b28f45d7bfc0e9932
Shwetank Agarwal <shwetank2003@gmail.com>
2026-01-13 21:27:10 +0530

    Added ws env

--- apps/web/app/hooks/useSocket.ts
+++ apps/web/app/hooks/useSocket.ts
@@ -1,5 +1,5 @@
 // ws.ts
-export const ws = new WebSocket(`ws://localhost:8081`);
+export const ws = new WebSocket(`${process.env.WS_URL}`);
 
 ws.onopen = () => console.log("WebSocket connected");
 ws.onclose = () => console.log("WebSocket disconnected");
